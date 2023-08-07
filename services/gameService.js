6a751aa0191101ea4f137df2a8ed330102432503
Anatoli Pavlov <18app81@gmail.com>
2023-08-07 18:34:52 +0300

    Add - edit, delete and get services

--- services/gameService.js
+++ services/gameService.js
@@ -4,3 +4,20 @@ const Game = require('../models/Game');
 exports.getAll = () => Game.find();
 
 exports.create = (gameData) => Game.create(gameData);
+
+exports.getById = (gameId) => Game.findById(gameId);
+
+exports.delete = (gameId) => Game.findByIdAndDelete(gameId);
+
+exports.edit = (gameId, gameData) => Game.findByIdAndUpdate(gameId, gameData);
+
+exports.isBuyer = async (gameId, userId) => {
+    const game = await Game.findById(gameId);
+    return game.boughtBy.includes(userId);
+};
+
+exports.buy = async (gameId, userId) => {
+    const game = await Game.findById(gameId);
+    game.boughtBy.push(userId);
+    game.save();
+};
