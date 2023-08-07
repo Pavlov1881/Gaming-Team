bc34316a09d18d21a976ec0f91e62ccb18842a88
Anatoli Pavlov <18app81@gmail.com>
2023-08-07 14:28:34 +0300

    Update a Game model validations

--- models/Game.js
+++ models/Game.js
@@ -3,22 +3,30 @@ const mongoose = require('mongoose');
 const gameSchema = new mongoose.Schema({
     name: {
         type: String,
+        minLength: 4,
         required: [true, 'Name is required!'],
     },
     image: {
         type: String,
         required: [true, 'Image is required!'],
+        validate: {
+            validator: (value) => /^https?:\/\//gi.test(value),
+            message: 'The Crypto Image URL must start with http:// or https://',
+        }
     },
     price: {
         type: Number,
+        min: [0, 'Price should be a positive number'],
         required: [true, 'Price is required!'],
     },
     description: {
         type: String,
+        minLength: [10, 'Description should be at least 10 characters long!'],
         required: [true, 'Description is required!'],
     },
     genre: {
         type: String,
+        minLength: [2, 'Genre should be at least 2 characters long!'],
         required: [true, 'Genre is required!'],
     },
     platform: {
